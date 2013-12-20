3f7ad659f33ed0c98f4cc42b1aadac5e6137da7b
Nick Doherty <nickdoherty@gmail.com>
2013-12-20 17:32:52 +1100

    disable post processing

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -42,13 +42,13 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  version :thumb do
-    process :resize_to_fill => [250, 250, 'North']
-  end
+  # version :thumb do
+  #   process :resize_to_fill => [250, 250, 'North']
+  # end
 
-  version :fit do
-    process :resize_to_fill => [800, 300, 'North']
-  end
+  # version :fit do
+  #   process :resize_to_fill => [800, 300, 'North']
+  # end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
