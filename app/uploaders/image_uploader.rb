2b9b4d25969fb3eece5d6cc99d31e05731dfcc71
Nick Doherty <nickdoherty@gmail.com>
2013-12-20 14:31:18 +1100

    fixing uploader a bit more

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,6 +6,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
 
+  include Sprockets::Helpers::RailsHelper
+  include Sprockets::Helpers::IsolateHelper
+
   # Choose what kind of storage to use for this uploader:
   # if Rails.env.development?
   #   storage :file
